56c907488d90213b92d8aa7a93ad89b30e7b32d5
zsr2531 <warhaamer1337@gmail.com>
2020-08-19 23:39:26 +0200

    Refactor

--- src/Bali/IO/ClassFileHeader.cs
+++ src/Bali/IO/ClassFileHeader.cs
@@ -5,14 +5,12 @@ namespace Bali.IO
         public readonly uint Magic;
         public readonly ushort Minor;
         public readonly ushort Major;
-        public readonly ushort ConstantPoolCount;
 
-        public ClassFileHeader(uint magic, ushort minor, ushort major, ushort constantPoolCount)
+        public ClassFileHeader(uint magic, ushort minor, ushort major)
         {
             Magic = magic;
             Minor = minor;
             Major = major;
-            ConstantPoolCount = constantPoolCount;
         }
     }
 }
