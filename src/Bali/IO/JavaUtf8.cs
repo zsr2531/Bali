29200df8f082af468b78696351a2bd9f5f23b271
zsr2531 <warhaamer1337@gmail.com>
2020-08-19 23:38:28 +0200

    Add xmldoc for JavaUtf8 class

--- src/Bali/IO/JavaUtf8.cs
+++ src/Bali/IO/JavaUtf8.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace Bali.IO
 {
+    /// <summary>
+    /// Provides <see cref="Decode"/> and <see cref="Encode"/> methods to consume Java's "modified UTF-8" data.
+    /// </summary>
     public static class JavaUtf8
     {
+        /// <summary>
+        /// Decodes <paramref name="length"/> amount of bytes from the <paramref name="stream"/> into a <see cref="string"/>.
+        /// </summary>
+        /// <param name="stream">The input <see cref="Stream"/> to read data from.</param>
+        /// <param name="length">The amount of <b><i><see cref="byte"/>s</i></b> to decode.</param>
+        /// <returns>The decoded <see cref="string"/>.</returns>
+        /// <exception cref="InvalidDataException">When the data read from the input stream isn't valid JavaUtf8.</exception>
         public static string Decode(Stream stream, int length)
         {
             // TODO: Maybe pool StringBuilder objects?
@@ -46,7 +56,7 @@ namespace Bali.IO
 
                 byte fourth = ReadByte(stream), fifth = ReadByte(stream), sixth = ReadByte(stream);
                 if (first != 0xED || fourth != 0xED)
-                    throw new Exception($"Invalid Java Utf-8: {first:X2}{second:X2}{third:X2}{fourth:X2}{fifth:X2}{sixth:X2}.");
+                    throw new InvalidDataException($"Invalid Java Utf-8: {first:X2}{second:X2}{third:X2}{fourth:X2}{fifth:X2}{sixth:X2}.");
                 
                 i += 3;
                 int sixByte = 0x1000 + ((second & 0x0F) << 16) + ((third & 0x3F) << 10) + ((fifth & 0x0F) << 6) + (sixth & 0x3F);
@@ -56,16 +66,21 @@ namespace Bali.IO
             return builder.ToString();
         }
 
+        /// <summary>
+        /// Encodes the given <paramref name="text"/> into JavaUtf8 and writes it to the <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to write the encoded bytes to.</param>
+        /// <param name="text">The <see cref="string"/> to encode.></param>
         public static void Encode(Stream stream, string text)
         {
-            //
+            throw new NotImplementedException();
         }
 
         private static byte ReadByte(Stream stream)
         {
             byte value = stream.ReadU1();
             if (value == 0 || value >= 0xF0 && value <= 0xFF)
-                throw new Exception($"Invalid byte in Java UTF-8: {value:X2}.");
+                throw new InvalidDataException($"Invalid byte in Java UTF-8: {value:X2}.");
 
             return value;
         }
