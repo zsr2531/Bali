05c3179c92eafa73a82c5a34fac89b4b32b8057f
zsr2531 <warhaamer1337@gmail.com>
2020-08-19 23:38:53 +0200

    Create custom exception for magic value mismatch

--- src/Bali/IO/ClassFileHeaderReader.cs
+++ src/Bali/IO/ClassFileHeaderReader.cs
@@ -1,26 +1,24 @@
-using System;
 using System.IO;
 
 namespace Bali.IO
 {
-    public class ClassFileReader
+    public class ClassFileHeaderReader
     {
         private readonly Stream _inputStream;
 
-        public ClassFileReader(Stream inputStream) => _inputStream = inputStream;
+        public ClassFileHeaderReader(Stream inputStream) => _inputStream = inputStream;
 
         public ClassFileHeader ReadHeader()
         {
             uint magic = _inputStream.ReadU4();
             
             if (magic != 0xCAFEBABE)
-                throw new Exception($"Incorrect magic value: 0x{magic:X4}.");
+                throw new InvalidMagicException();
 
             ushort minor = _inputStream.ReadU2();
             ushort major = _inputStream.ReadU2();
-            ushort constantPoolCount = _inputStream.ReadU2();
 
-            return new ClassFileHeader(magic, minor, major, constantPoolCount);
+            return new ClassFileHeader(magic, minor, major);
         }
     }
 }
---
+++ src/Bali/IO/InvalidMagicException.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Bali.IO
+{
+    /// <summary>
+    /// Thrown when the first 4 bytes of a <see cref="ClassFile"/> isn't <b>0xCAFEBABE</b>.
+    /// </summary>
+    public sealed class InvalidMagicException : Exception
+    {
+        /// <summary>
+        /// Creates a new <see cref="InvalidMagicException"/>.
+        /// </summary>
+        public InvalidMagicException()
+            : base("The magic value wasn't valid.") { }
+    }
+}
