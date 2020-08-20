dc35963c5bb91e7c534d6fb5789762bd013da29c
zsr2531 <warhaamer1337@gmail.com>
2020-08-20 22:53:06 +0200

    Clean up threadlocal code

--- src/Bali/IO/JavaUtf8.cs
+++ src/Bali/IO/JavaUtf8.cs
@@ -10,7 +10,7 @@ namespace Bali.IO
     /// </summary>
     public static class JavaUtf8
     {
-        private static readonly ThreadLocal<StringBuilder> Builder = new ThreadLocal<StringBuilder>(() => new StringBuilder());
+        private static readonly ThreadLocal<StringBuilder> Builder = new ThreadLocal<StringBuilder>();
         
         /// <summary>
         /// Decodes <paramref name="length"/> amount of bytes from the <paramref name="stream"/> into a <see cref="string"/>.
