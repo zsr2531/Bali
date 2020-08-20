8a5ffa548bf7ee32c656efe27267d301911c6faa
zsr2531 <warhaamer1337@gmail.com>
2020-08-20 22:51:44 +0200

    Add xmldoc to class file header

--- src/Bali/IO/ClassFileHeaderReader.cs
+++ src/Bali/IO/ClassFileHeaderReader.cs
@@ -1,15 +1,32 @@
+using System;
 using System.IO;
 
 namespace Bali.IO
 {
-    public class ClassFileHeaderReader
+    /// <summary>
+    /// Reads the magic value, the major- and the minor version from the given <see cref="Stream"/>.
+    /// </summary>
+    public readonly struct ClassFileHeaderReader
     {
-        private readonly Stream _inputStream;
+        private readonly Stream? _inputStream;
 
+        /// <summary>
+        /// Creates a new <see cref="ClassFileHeaderReader"/>.
+        /// </summary>
+        /// <param name="inputStream">The input <see cref="Stream"/> to read data from.</param>
         public ClassFileHeaderReader(Stream inputStream) => _inputStream = inputStream;
 
+        /// <summary>
+        /// Parses the <see cref="ClassFileHeader"/> from the input <see cref="Stream"/>.
+        /// </summary>
+        /// <returns>The <see cref="ClassFileHeader"/> holding the magic value, the major- and minor versions.</returns>
+        /// <exception cref="ArgumentException">When the input <see cref="Stream"/> is <i>null</i>.</exception>
+        /// <exception cref="InvalidMagicException">When the magic value isn't <b>0xCAFEBABE</b>.</exception>
         public ClassFileHeader ReadHeader()
         {
+            if (_inputStream is null)
+                throw new ArgumentException("No input stream was provided.");
+            
             uint magic = _inputStream.ReadU4();
             
             if (magic != 0xCAFEBABE)
@@ -17,8 +34,9 @@ namespace Bali.IO
 
             ushort minor = _inputStream.ReadU2();
             ushort major = _inputStream.ReadU2();
+            ushort constantPoolCount = _inputStream.ReadU2();
 
-            return new ClassFileHeader(magic, minor, major);
+            return new ClassFileHeader(magic, minor, major, constantPoolCount);
         }
     }
 }
