eb58177aedbdd1e5631a96e67a8bef11d5811b4f
zsr2531 <warhaamer1337@gmail.com>
2020-08-21 01:01:46 +0200

    Create token models

---
+++ src/Bali/IO/Descriptors/TokenKind.cs
@@ -0,0 +1,18 @@
+namespace Bali.IO.Descriptors
+{
+    public enum TokenKind
+    {
+        LeftParenthesis,
+        RightParenthesis,
+        LeftBracket,
+        Byte,
+        Char,
+        Double,
+        Float,
+        Int,
+        Long,
+        Short,
+        Boolean,
+        ClassName
+    }
+}
