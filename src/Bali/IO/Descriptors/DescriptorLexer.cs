11ad36e44d1155ce97a8a529a5b15a36fc24552d
zsr2531 <warhaamer1337@gmail.com>
2020-08-21 10:46:34 +0200

    Refactor lexer and add custom exception type

--- src/Bali/IO/Descriptors/DescriptorLexer.cs
+++ src/Bali/IO/Descriptors/DescriptorLexer.cs
@@ -3,43 +3,63 @@ using System.Collections.Generic;
 
 namespace Bali.IO.Descriptors
 {
+    /// <summary>
+    /// A simple lexer that turns Java descriptor strings into a token stream.
+    /// </summary>
     public class DescriptorLexer
     {
         private readonly ReadOnlyMemory<char> _text;
 
         private int _position;
 
+        /// <summary>
+        /// Creates a new <see cref="DescriptorLexer"/> with the given input <paramref name="text"/>
+        /// </summary>
+        /// <param name="text">The input to lex.</param>
         public DescriptorLexer(ReadOnlyMemory<char> text) =>
             _text = text;
 
         private char Current => _text.Span[_position];
 
+        /// <summary>
+        /// Turns the input text into a stream of <see cref="DescriptorToken"/>s.
+        /// </summary>
+        /// <returns>A stream of <see cref="DescriptorToken"/>s.</returns>
+        /// <exception cref="DescriptorLexerException">
+        /// When during the lexical analysis of a reference token, the input unexpectedly ends.
+        /// </exception>
         public IEnumerable<DescriptorToken> Lex()
         {
-            if (Current == '\0')
+            switch (Current)
             {
-                var span = new TextSpan(_position, _position);
-                var token = new DescriptorToken(span, DescriptorTokenKind.EndOfFile, ReadOnlyMemory<char>.Empty);
-                yield return token;
-                yield break;
-            }
-
-            if (Current == 'L')
-            {
-                int start = _position;
-                while (Next() != ';')
+                case '\0':
                 {
-                    if (Current == '\0')
-                        throw new Exception("Unexpected end of input.");
+                    var span = new TextSpan(_position, _position);
+                    var token = new DescriptorToken(span, DescriptorTokenKind.EndOfFile, ReadOnlyMemory<char>.Empty);
+                    
+                    yield return token;
+                    yield break;
                 }
+
+                case 'L':
+                {
+                    int start = _position;
+                    while (Next() != ';')
+                    {
+                        if (Current == '\0')
+                            throw new DescriptorLexerException("Unexpected end of input.");
+                    }
                 
-                var span = new TextSpan(start, _position);
-                var text = _text.Slice(start, _position);
-                yield return new DescriptorToken(span, DescriptorTokenKind.ClassName, text);
-            }
-            else
-            {
-                yield return SingleCharacter();
+                    var span = new TextSpan(start, _position);
+                    var text = _text.Slice(start, _position);
+                    
+                    yield return new DescriptorToken(span, DescriptorTokenKind.ClassName, text);
+                    break;
+                }
+
+                default:
+                    yield return SingleCharacter();
+                    break;
             }
         }
 
