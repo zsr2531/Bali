2a30b0b5e7b647ace196f7603fc71da8d5961133
zsr2531 <warhaamer1337@gmail.com>
2020-08-29 01:34:33 +0200

    Add lexer tests for identifiers

---
+++ test/Bali.Descriptors.Tests/Lexer/IdentifierTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Bali.IO.Descriptors;
+using Xunit;
+
+namespace Bali.Descriptors.Tests.Lexer
+{
+    public class IdentifierTests
+    {
+        [Theory]
+        [InlineData("java")]
+        [InlineData("bali")]
+        [InlineData("class")]
+        public void ValidIdentifier(string input)
+        {
+            var lexer = new DescriptorLexer(input.AsMemory());
+            var tokens = Utils.GetAllTokens(lexer.Lex());
+            
+            Assert.Equal(2, tokens.Count);
+            Assert.Equal(DescriptorTokenKind.Identifier, tokens[0].Kind);
+            Assert.Equal(input, tokens[0].Value.ToString());
+        }
+
+        [Fact]
+        public void ValidIdentifierButContainsAngledBrackets()
+        {
+            var lexer = new DescriptorLexer("<init>".AsMemory());
+            var tokens = Utils.GetAllTokens(lexer.Lex());
+            
+            Assert.Equal(4, tokens.Count);
+            Assert.True(tokens[0].Kind == DescriptorTokenKind.LeftAngledBracket);
+            Assert.True(tokens[1].Kind == DescriptorTokenKind.Identifier);
+            Assert.True(tokens[2].Kind == DescriptorTokenKind.RightAngledBracket);
+        }
+    }
+}
