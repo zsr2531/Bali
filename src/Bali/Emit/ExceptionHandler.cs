2473c783f4fc1c039d551bacf97cc268feccb357
zsr2531 <warhaamer1337@gmail.com>
2020-09-14 20:59:56 +0200

    Create low level exception handler model

---
+++ src/Bali/Emit/ExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Bali.Emit
+{
+    public sealed class ExceptionHandler
+    {
+        public ushort TryStart
+        {
+            get;
+            set;
+        }
+
+        public ushort TryEnd
+        {
+            get;
+            set;
+        }
+
+        public ushort HandlerStart
+        {
+            get;
+            set;
+        }
+
+        public ushort CatchType
+        {
+            get;
+            set;
+        }
+    }
+}
