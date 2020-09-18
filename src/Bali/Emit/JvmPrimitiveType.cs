36cd1c8cd8937314a0f7caebc2cfe1a4fd2fc7fd
zsr2531 <warhaamer1337@gmail.com>
2020-09-18 17:22:51 +0200

    Create primitive types JVM enumeration

---
+++ src/Bali/Emit/JvmPrimitiveType.cs
@@ -0,0 +1,14 @@
+﻿namespace Bali.Emit
+{
+    public enum JvmPrimitiveType
+    {
+        Boolean = 4,
+        Char = 5,
+        Float = 6,
+        Double = 7,
+        Byte = 8,
+        Short = 9,
+        Int = 10,
+        Long = 11
+    }
+}
