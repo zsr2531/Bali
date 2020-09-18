4d11d61ddde6f3c150036f25e918e4569e43d92e
zsr2531 <warhaamer1337@gmail.com>
2020-09-18 17:23:09 +0200

    Create basic code attribute model

---
+++ src/Bali/Metadata/Attributes/CodeAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Bali.Emit;
+
+namespace Bali.Metadata.Attributes
+{
+    public sealed class CodeAttribute : Attribute
+    {
+        public CodeAttribute(ushort nameIndex)
+            : base(nameIndex) { }
+
+        public ushort MaxStack
+        {
+            get;
+        }
+
+        public ushort MaxLocals
+        {
+            get;
+        }
+
+        public IReadOnlyList<byte> Bytecode
+        {
+            get;
+        }
+
+        public IReadOnlyList<ExceptionHandler> ExceptionHandlers
+        {
+            get;
+        }
+
+        public IReadOnlyList<Attribute> Attributes
+        {
+            get;
+        }
+    }
+}
