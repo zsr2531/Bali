19984235d6aa4aefb2c7283c32d71061c8b0f6e9
zsr2531 <warhaamer1337@gmail.com>
2020-09-19 16:47:03 +0200

    Provide "real" primitives

--- src/Bali/Emit/JvmPrimitiveType.cs
+++ src/Bali/Emit/JvmPrimitiveType.cs
@@ -1,14 +1,48 @@
 ﻿namespace Bali.Emit
 {
+    /// <summary>
+    /// The 8 "real" primitive types that the JVM supports.
+    /// </summary>
     public enum JvmPrimitiveType
     {
+        /// <summary>
+        /// The <c>boolean</c> primitive.
+        /// </summary>
         Boolean = 4,
+
+        /// <summary>
+        /// The <c>char</c> primitive.
+        /// </summary>
         Char = 5,
+
+        /// <summary>
+        /// The <c>float</c> primitive.
+        /// </summary>
         Float = 6,
+
+        /// <summary>
+        /// The <c>double</c> primitive.
+        /// </summary>
         Double = 7,
+
+        /// <summary>
+        /// The <c>byte</c> primitive.
+        /// </summary>
         Byte = 8,
+
+        /// <summary>
+        /// The <c>short</c> primitive.
+        /// </summary>
         Short = 9,
+
+        /// <summary>
+        /// The <c>int</c> primitive.
+        /// </summary>
         Int = 10,
+
+        /// <summary>
+        /// The <c>long</c> primitive.
+        /// </summary>
         Long = 11
     }
 }
