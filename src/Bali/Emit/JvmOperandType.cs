5c93f138a1f815690935d819af8f33ba1cf783d6
zsr2531 <warhaamer1337@gmail.com>
2020-09-19 16:47:31 +0200

    Rewrite operand type enumeration from scratch

--- src/Bali/Emit/JvmOperandType.cs
+++ src/Bali/Emit/JvmOperandType.cs
@@ -3,31 +3,7 @@
     public enum JvmOperandType : byte
     {
         None,
-        
         Byte,
-        Short,
-        
-        ConstantIndex,
-        WideConstantIndex,
-        
-        LocalIndex,
-        LocalConst,
-        
-        BranchOffset,
-        WideBranchOffset,
-        
-        TableSwitch,
-        LookupSwitch,
-        
-        WideIndexCountZero,
-        WideIndexByte,
-        
-        PrimitiveType,
-        
-        FieldIndex,
-        MethodIndex,
-        ClassIndex,
-
-        DynamicIndex
+        Short
     }
 }
