26b00df6a9f16193df0d6654efdd6baba5b8fe08
zsr2531 <warhaamer1337@gmail.com>
2020-09-19 16:45:39 +0200

    Rename exception handler

---
+++ src/Bali/Emit/JvmExceptionHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace Bali.Emit
+{
+    /// <summary>
+    /// Represents a JVM exception handler.
+    /// </summary>
+    public struct JvmExceptionHandler
+    {
+        /// <summary>
+        /// Gets the index of the first instruction to protect.
+        /// </summary>
+        public ushort TryStart
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the index of the last instruction to protect.
+        /// </summary>
+        /// <remarks>Note that this is an exclusive index.</remarks>
+        public ushort TryEnd
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the index of the handler block's first instruction.
+        /// </summary>
+        public ushort HandlerStart
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets the index into the <see cref="ConstantPool" /> describing the type of the exception to catch
+        /// or 0 if the exception handler is a <c>finally</c> block.
+        /// </summary>
+        public ushort CatchType
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() => TryStart + TryEnd + HandlerStart + CatchType;
+    }
+}
