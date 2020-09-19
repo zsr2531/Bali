a48e9febc571155facc91e859807c88a764a58a4
zsr2531 <warhaamer1337@gmail.com>
2020-09-19 16:47:38 +0200

    Create a basic instruction model

---
+++ src/Bali/Emit/JvmInstruction.cs
@@ -0,0 +1,68 @@
+using System;
+using Bali.Metadata.Attributes;
+
+namespace Bali.Emit
+{
+    /// <summary>
+    /// Represents a JVM instruction in a method's <see cref="CodeAttribute"/>.
+    /// </summary>
+    public sealed class JvmInstruction : IEquatable<JvmInstruction>
+    {
+        /// <summary>
+        /// Creates a new <see cref="JvmInstruction"/>.
+        /// </summary>
+        /// <param name="opCode">The <see cref="JvmOpCode"/> of the instruction.</param>
+        /// <param name="operand">The instruction's operand.</param>
+        public JvmInstruction(JvmOpCode opCode, object? operand = null)
+        {
+            OpCode = opCode;
+            Operand = null;
+        }
+                
+        /// <summary>
+        /// Gets or sets the opcode of the instruction.
+        /// </summary>
+        public JvmOpCode OpCode
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// Gets or sets the operand of the instruction.
+        /// </summary>
+        public object? Operand
+        {
+            get;
+            set;
+        }
+        
+        /// <inheritdoc />
+        public bool Equals(JvmInstruction other) => OpCode == other.OpCode && object.Equals(Operand, other.Operand);
+        
+        /// <inheritdoc />
+        public override bool Equals(object? obj) => obj is JvmInstruction instruction && Equals(instruction);
+        
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(OpCode, Operand);
+
+        /// <inheritdoc />
+        public override string ToString() => $"{OpCode}{(Operand is null ? "" : " ")}{Operand}";
+        
+        /// <summary>
+        /// Determines whether two <see cref="JvmInstruction"/>s are equal.
+        /// </summary>
+        /// <param name="left">The left hand side of the comparison.</param>
+        /// <param name="right">The right hand side of the comparison.</param>
+        /// <returns>Whether the left and right hand side are equal.</returns>
+        public static bool operator ==(JvmInstruction left, JvmInstruction right) => object.Equals(left, right);
+
+        /// <summary>
+        /// Determines whether two <see cref="JvmInstruction"/>s are not equal.
+        /// </summary>
+        /// <param name="left">The left hand side of the comparison.</param>
+        /// <param name="right">The right hand side of the comparison.</param>
+        /// <returns>Whether the left and right hand side are not equal.</returns>
+        public static bool operator !=(JvmInstruction left, JvmInstruction right) => !(left == right);
+    }
+}
