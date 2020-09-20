ff013f800d8e646ecffabc7860f64338c4a44666
zsr2531 <warhaamer1337@gmail.com>
2020-09-20 20:39:02 +0200

    Add comments to Field-, Method- and InterfaceMethodref constant models

--- src/Bali/IO/Constants/InterfaceMethodrefConstant.cs
+++ src/Bali/IO/Constants/InterfaceMethodrefConstant.cs
@@ -2,8 +2,16 @@ using System.IO;
 
 namespace Bali.IO.Constants
 {
+    /// <summary>
+    /// Represents a method of an interface in a <see cref="ClassFile"/>.
+    /// </summary>
     public class InterfaceMethodrefConstant : Constant
     {
+        /// <summary>
+        /// Creates a new <see cref="InterfaceMethodrefConstant"/>.
+        /// </summary>
+        /// <param name="classIndex">The index into the <see cref="ConstantPool"/> to a <see cref="ClassConstant"/> (must be an interface) that this method is the member of.</param>
+        /// <param name="nameAndTypeIndex">The index into the <see cref="ConstantPool"/> to a <see cref="NameAndTypeConstant"/> that indicates the name and the descriptor of this method.</param>
         public InterfaceMethodrefConstant(ushort classIndex, ushort nameAndTypeIndex)
             : base(ConstantKind.InterfaceMethodref)
         {
@@ -11,16 +19,30 @@ namespace Bali.IO.Constants
             NameAndTypeIndex = nameAndTypeIndex;
         }
 
+        /// <summary>
+        /// Gets or sets the index into the <see cref="ConstantPool"/> to a <see cref="ClassConstant"/> (must be an interface) that this method is the member of.
+        /// </summary>
         public ushort ClassIndex
         {
             get;
+            set;
         }
 
+        /// <summary>
+        /// Gets or sets the index into the <see cref="ConstantPool"/> to a <see cref="NameAndTypeConstant"/> that indicates the name and the descriptor of this method.
+        /// </summary>
+        /// <value></value>
         public ushort NameAndTypeIndex
         {
             get;
+            set;
         }
         
+        /// <summary>
+        /// Parses a <see cref="InterfaceMethodrefConstant"/> from the given input <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">The input <see cref="Stream"/> to read data from.</param>
+        /// <returns>The parsed <see cref="InterfaceMethodrefConstant"/>.</returns>
         public static InterfaceMethodrefConstant Create(Stream stream) =>
             new InterfaceMethodrefConstant(stream.ReadU2(), stream.ReadU2());
     }
--- src/Bali/IO/Constants/MethodrefConstant.cs
+++ src/Bali/IO/Constants/MethodrefConstant.cs
@@ -2,8 +2,16 @@ using System.IO;
 
 namespace Bali.IO.Constants
 {
+    /// <summary>
+    /// Represents a method in the <see cref="ClassFile"/>.
+    /// </summary>
     public class MethodrefConstant : Constant
     {
+        /// <summary>
+        /// Creates a new <see cref="MethodrefConstant"/>.
+        /// </summary>
+        /// <param name="classIndex"></param>
+        /// <param name="nameAndTypeIndex"></param>
         public MethodrefConstant(ushort classIndex, ushort nameAndTypeIndex)
             : base(ConstantKind.Methodref)
         {
@@ -11,16 +19,29 @@ namespace Bali.IO.Constants
             NameAndTypeIndex = nameAndTypeIndex;
         }
 
+        /// <summary>
+        /// Gets or sets the index into the <see cref="ConstantPool"/> to a <see cref="ClassConstant"/> that this method is a member of.
+        /// </summary>
         public ushort ClassIndex
         {
             get;
+            set;
         }
 
+        /// <summary>
+        /// Gets or sets the index into the <see cref="ConstantPool"/> to a <see cref="NameAndTypeConstant"/> that indicates the name and the descriptor of this method.
+        /// </summary>
         public ushort NameAndTypeIndex
         {
             get;
+            set;
         }
         
+        /// <summary>
+        /// Parses a <see cref="MethodrefConstant"/> from the given input <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">The input <see cref="Stream"/> to read data from.</param>
+        /// <returns>The parsed <see cref="MethodrefConstant"/>.</returns>
         public static MethodrefConstant Create(Stream stream) => new MethodrefConstant(stream.ReadU2(), stream.ReadU2());
     }
 }
