dae478a808aa0a9418c7eaad3fffd71b54661e4c
zsr2531 <warhaamer1337@gmail.com>
2020-09-20 20:38:41 +0200

    Add comments for base class and enumeration

--- src/Bali/IO/Constants/Constant.cs
+++ src/Bali/IO/Constants/Constant.cs
@@ -1,9 +1,19 @@
 namespace Bali.IO.Constants
 {
+    /// <summary>
+    /// A base class for constants in the <see cref="ConstantPool"/>.
+    /// </summary>
     public abstract class Constant
     {
+        /// <summary>
+        /// Initializes the <see cref="Tag"/>.
+        /// </summary>
+        /// <param name="tag">The <see cref="ConstantKind"/>.</param>
         protected Constant(ConstantKind tag) => Tag = tag;
 
+        /// <summary>
+        /// Gets the the type of the constant.
+        /// </summary>
         public ConstantKind Tag
         {
             get;
