0c118f9e705030475b4e2958b3ba9b782ec066eb
zsr2531 <warhaamer1337@gmail.com>
2020-11-12 11:38:38 +0100

    Add extra Stream extension methods

--- src/Bali/IO/StreamExtensions.cs
+++ src/Bali/IO/StreamExtensions.cs
@@ -22,5 +22,13 @@ namespace Bali.IO
         
         internal static ulong ReadU8(this Stream stream) =>
             (ulong) stream.ReadU4() << 32 | stream.ReadU4();
+
+        internal static sbyte ReadI1(this Stream stream) => (sbyte) stream.ReadU1();
+
+        internal static short ReadI2(this Stream stream) => (short) stream.ReadU2();
+
+        internal static int ReadI4(this Stream stream) => (int) stream.ReadU4();
+
+        internal static long ReadI8(this Stream stream) => (long) stream.ReadU8();
     }
 }
