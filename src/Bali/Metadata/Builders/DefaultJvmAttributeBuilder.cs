8725cb08af14cfb5c17f8131a21381f687733811
zsr2531 <warhaamer1337@gmail.com>
2020-12-05 23:45:42 +0100

    Create default builder implementation

---
+++ src/Bali/Metadata/Builders/DefaultJvmAttributeBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Bali.IO;
+
+namespace Bali.Metadata.Builders
+{
+    public sealed class DefaultJvmAttributeBuilder : IJvmAttributeBuilder
+    {
+        /// <inheritdoc />
+        public string Name => throw new NotSupportedException();
+
+        /// <inheritdoc />
+        public void BuildName(Stream stream, JvmAttribute attribute) => stream.WriteU2(attribute.NameIndex);
+
+        /// <inheritdoc />
+        public void BuildBody(Stream stream, JvmAttribute attribute)
+        {
+            if (attribute.Data is null)
+                throw new ArgumentOutOfRangeException(nameof(attribute));
+                
+            stream.Write(attribute.Data, 0, attribute.Data.Length);
+        }
+    }
+}
