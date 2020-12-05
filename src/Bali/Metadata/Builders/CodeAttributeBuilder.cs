c36408638f91c9d032f5813b556412ce73df348a
zsr2531 <warhaamer1337@gmail.com>
2020-12-05 23:45:55 +0100

    Add code attribute builder

---
+++ src/Bali/Metadata/Builders/CodeAttributeBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using Bali.Emit;
+using Bali.IO;
+using Bali.Metadata.Attributes;
+
+namespace Bali.Metadata.Builders
+{
+    public class CodeAttributeBuilder : JvmAttributeBuilderBase<CodeAttribute>
+    {
+        private readonly IJvmBytecodeAssembler _assembler;
+
+        public CodeAttributeBuilder(IJvmAttributeDirector director)
+            : this(director, JvmBytecodeAssembler.Instance) { }
+
+        public CodeAttributeBuilder(IJvmAttributeDirector director, IJvmBytecodeAssembler assembler)
+            : base(director)
+        {
+            _assembler = assembler;
+        }
+
+        /// <inheritdoc />
+        public override string Name => "Code";
+
+        /// <inheritdoc />
+        protected override void BuildBody(Stream stream, CodeAttribute attribute)
+        {
+            stream.WriteU2(attribute.MaxStack);
+            stream.WriteU2(attribute.MaxLocals);
+            
+            BuildBytecode(stream, attribute);
+            BuildExceptionHandlers(stream, attribute);
+            
+            stream.WriteU2((ushort) attribute.Attributes.Count);
+            foreach (var nestedAttribute in attribute.Attributes)
+                Director.ConstructAttribute(nestedAttribute);
+        }
+
+        private void BuildBytecode(Stream stream, CodeAttribute attribute)
+        {
+            using var ms = new MemoryStream();
+            _assembler.Assemble(attribute.Instructions, ms);
+
+            stream.WriteU4((uint) ms.Length);
+            ms.CopyTo(stream);
+        }
+
+        private static void BuildExceptionHandlers(Stream stream, CodeAttribute attribute)
+        {
+            foreach (var exceptionHandler in attribute.ExceptionHandlers)
+            {
+                stream.WriteU2(exceptionHandler.TryStart);
+                stream.WriteU2(exceptionHandler.TryEnd);
+                stream.WriteU2(exceptionHandler.HandlerStart);
+                stream.WriteU2(exceptionHandler.CatchType);
+            }
+        }
+    }
+}
