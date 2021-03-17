87a55276b54c73abbbe77d4435fe092eecc90005
zsr2531 <warhaamer1337@gmail.com>
2021-03-17 10:15:17 +0100

    Refactor metadata body reading and writing

--- src/Bali/IO/MetadataBodyReader.cs
+++ src/Bali/IO/MetadataBodyReader.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Runtime.CompilerServices;
 using Bali.Attributes;
 using Bali.Attributes.Readers;
@@ -8,39 +7,35 @@ using Bali.Attributes.Readers;
 namespace Bali.IO
 {
     /// <summary>
-    /// Reads the interface indices, fields, methods and attributes from a given <see cref="Stream"/>.
+    /// Reads the interface indices, fields, methods and attributes from a given input <see cref="IBigEndianReader"/>.
     /// </summary>
-    public readonly struct MetadataBodyReader
+    public readonly ref struct MetadataBodyReader
     {
-        private readonly Stream? _inputStream;
-        private readonly IJvmAttributeReaderFacade? _attributeFactory;
+        private readonly IBigEndianReader _reader;
+        private readonly IJvmAttributeReaderFacade _facade;
 
         /// <summary>
         /// Creates a new <see cref="MetadataBodyReader"/>.
         /// </summary>
-        /// <param name="inputStream">The input <see cref="Stream"/> to read data from.</param>
+        /// <param name="reader">The input <see cref="IBigEndianReader"/> to read data from.</param>
         /// <param name="constantPool">The <see cref="ConstantPool"/> to resolve constant pool indices with.</param>
-        public MetadataBodyReader(Stream inputStream, ConstantPool constantPool)
+        public MetadataBodyReader(IBigEndianReader reader, ConstantPool constantPool)
         {
-            _inputStream = inputStream;
-            _attributeFactory = new JvmAttributeReaderFacade(inputStream, constantPool);
+            _reader = reader;
+            _facade = new JvmAttributeReaderFacade(reader, constantPool);
         }
 
         /// <summary>
-        /// Parses the <see cref="MetadataBody"/> from the input <see cref="Stream"/>.
+        /// Parses the <see cref="MetadataBody"/> from the input <see cref="IBigEndianReader"/>.
         /// </summary>
         /// <returns>The parsed <see cref="MetadataBody"/>.</returns>
-        /// <exception cref="ArgumentException">When the input <see cref="Stream"/> is <i>null</i>.</exception>
         public MetadataBody ReadMetadataBody()
         {
-            if (_inputStream is null)
-                throw new ArgumentException("No input stream was provided.");
-
-            ushort interfacesCount = _inputStream.ReadU2();
+            ushort interfacesCount = _reader.ReadU2();
             var interfaces = new List<ushort>(interfacesCount);
 
             for (int i = 0; i < interfacesCount; i++)
-                interfaces.Add(_inputStream.ReadU2());
+                interfaces.Add(_reader.ReadU2());
 
             var fields = ReadInfo<JvmFieldInfo, FieldAccessFlags>((f, n, d, a) => new JvmFieldInfo(f, n, d, a));
             var methods = ReadInfo<JvmMethodInfo, MethodAccessFlags>((f, n, d, a) => new JvmMethodInfo(f, n, d, a));
@@ -51,17 +46,17 @@ namespace Bali.IO
 
         private IList<TType> ReadInfo<TType, TFlags>(Func<TFlags, ushort, ushort, IList<JvmAttribute>, TType> factory)
         {
-            ushort count = _inputStream!.ReadU2();
+            ushort count = _reader.ReadU2();
             if (count == 0)
                 return new List<TType>();
 
             var buffer = new List<TType>(count);
             for (int i = 0; i < count; i++)
             {
-                ushort rawFlags = _inputStream!.ReadU2();
+                ushort rawFlags = _reader!.ReadU2();
                 var flags = Unsafe.As<ushort, TFlags>(ref rawFlags);
-                ushort nameIndex = _inputStream!.ReadU2();
-                ushort descriptorIndex = _inputStream!.ReadU2();
+                ushort nameIndex = _reader!.ReadU2();
+                ushort descriptorIndex = _reader!.ReadU2();
                 var attributes = ReadAttributes();
                 buffer.Add(factory(flags, nameIndex, descriptorIndex, attributes));
             }
@@ -71,13 +66,13 @@ namespace Bali.IO
 
         private IList<JvmAttribute> ReadAttributes()
         {
-            ushort attributesCount = _inputStream!.ReadU2();
+            ushort attributesCount = _reader.ReadU2();
             if (attributesCount == 0)
                 return new List<JvmAttribute>();
             
             var attributes = new List<JvmAttribute>(attributesCount);
             for (int i = 0; i < attributesCount; i++)
-                attributes.Add(_attributeFactory!.ReadAttribute());
+                attributes.Add(_facade.ReadAttribute());
 
             return attributes;
         }
