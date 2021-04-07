67195b70afb0711528f2046a1a3c00f19c962d74
zsr2531 <warhaamer1337@gmail.com>
2021-04-07 21:23:23 +0200

    Add xml documentation

--- src/Bali/Attributes/StackMapTableAttribute.StackMapFrame.cs
+++ src/Bali/Attributes/StackMapTableAttribute.StackMapFrame.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace Bali.Attributes
 {
+    /// <summary>
+    /// Provides a contract for stack map frames.
+    /// </summary>
     public abstract class StackMapFrame
     {
         private protected StackMapFrame(byte tag)
@@ -10,19 +13,33 @@ namespace Bali.Attributes
             Tag = tag;
         }
         
+        /// <summary>
+        /// Gets the tag.
+        /// </summary>
         public byte Tag
         {
             get;
         }
         
+        /// <summary>
+        /// Gets the offset to the next frame in the bytecode.
+        /// </summary>
         public abstract ushort OffsetDelta
         {
             get;
         }
     }
     
+    /// <summary>
+    /// Indicates that the frame is the same as the previous one.
+    /// </summary>
     public sealed class SameFrame : StackMapFrame
     {
+        /// <summary>
+        /// Creates a new <see cref="SameFrame"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is greater than 63.</exception>
         public SameFrame(byte tag)
             : base(tag)
         {
@@ -34,8 +51,17 @@ namespace Bali.Attributes
         public override ushort OffsetDelta => Tag;
     }
     
+    /// <summary>
+    /// Indicates that the frame is the same as the previous one, except the top stack item.
+    /// </summary>
     public sealed class SameLocals1StackItemFrame : StackMapFrame
     {
+        /// <summary>
+        /// Creates a new <see cref="SameLocals1StackItemFrame"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <param name="stackTop">The type of the top item on the stack.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is less than 64 or greater than 127.</exception>
         public SameLocals1StackItemFrame(byte tag, VerificationInfo stackTop)
             : base(tag)
         {
@@ -48,15 +74,29 @@ namespace Bali.Attributes
         /// <inheritdoc />
         public override ushort OffsetDelta => (ushort) (Tag - 64);
         
+        /// <summary>
+        /// Gets the type of the top item on the stack.
+        /// </summary>
         public VerificationInfo StackTop
         {
             get;
         }
     }
     
+    /// <summary>
+    /// Indicates that the frame is the same as the previous one, except the top stack item but it has an explicit
+    /// offset for a bigger "range".
+    /// </summary>
     public sealed class SameLocals1StackItemFrameExtended : StackMapFrame
     {
-        internal SameLocals1StackItemFrameExtended(byte tag, ushort offsetDelta, VerificationInfo stackTop)
+        /// <summary>
+        /// Creates a new <see cref="SameLocals1StackItemFrameExtended"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <param name="offsetDelta">The offset to the next frame in the bytecode.</param>
+        /// <param name="stackTop">The type of the top item on the stack.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is not 247.</exception>
+        public SameLocals1StackItemFrameExtended(byte tag, ushort offsetDelta, VerificationInfo stackTop)
             : base(tag)
         {
             if (tag != 247)
@@ -72,14 +112,26 @@ namespace Bali.Attributes
             get;
         }
         
+        /// <summary>
+        /// Gets the type of the top item on the stack.
+        /// </summary>
         public VerificationInfo StackTop
         {
             get;
         }
     }
     
+    /// <summary>
+    /// Indicates that the frame is the same as the previous one with the last <i>k</i> locals dropped.
+    /// </summary>
     public sealed class ChopFrame : StackMapFrame
     {
+        /// <summary>
+        /// Creates a new <see cref="ChopFrame"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <param name="offsetDelta">The offset to the next frame in the bytecode.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is less than 248 or greater than 250.</exception>
         public ChopFrame(byte tag, ushort offsetDelta)
             : base(tag)
         {
@@ -89,6 +141,9 @@ namespace Bali.Attributes
             OffsetDelta = offsetDelta;
         }
 
+        /// <summary>
+        /// Gets the number of locals to drop from the previous frame, starting from the end.
+        /// </summary>
         public int LastLocalsToDrop => 251 - Tag;
 
         /// <inheritdoc />
@@ -98,8 +153,17 @@ namespace Bali.Attributes
         }
     }
     
+    /// <summary>
+    /// Indicates that the frame is the same as the previous one, but it has an explicit offset for a bigger "range".
+    /// </summary>
     public sealed class SameFrameExtended : StackMapFrame
     {
+        /// <summary>
+        /// Creates a new <see cref="ChopFrame"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <param name="offsetDelta">The offset to the next frame in the bytecode.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is not 251.</exception>
         public SameFrameExtended(byte tag, ushort offsetDelta)
             : base(tag)
         {
@@ -116,8 +180,18 @@ namespace Bali.Attributes
         }
     }
     
+    /// <summary>
+    /// Indicates that the frame is the same as the previous one, except new locals are added to the stack frame.
+    /// </summary>
     public sealed class AppendFrame : StackMapFrame
     {
+        /// <summary>
+        /// Creates a new <see cref="AppendFrame"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <param name="offsetDelta">The offset to the next frame in the bytecode.</param>
+        /// <param name="newLocals">The new locals to append to the previous frame.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is less than 252 or greater than 254.</exception>
         public AppendFrame(byte tag, ushort offsetDelta, IList<VerificationInfo> newLocals)
             : base(tag)
         {
@@ -134,14 +208,28 @@ namespace Bali.Attributes
             get;
         }
         
+        /// <summary>
+        /// Gets the new locals to append to the previous frame.
+        /// </summary>
         public IList<VerificationInfo> NewLocals
         {
             get;
         }
     }
     
+    /// <summary>
+    /// Indicates that the stack frame is completely redefined compared to the previous one.
+    /// </summary>
     public sealed class FullFrame : StackMapFrame
     {
+        /// <summary>
+        /// Creates a new <see cref="FullFrame"/>.
+        /// </summary>
+        /// <param name="tag">The tag.</param>
+        /// <param name="offsetDelta">The offset to the next frame in the bytecode.</param>
+        /// <param name="locals">The locals in the stack frame.</param>
+        /// <param name="stack">The stack items of the stack frame.</param>
+        /// <exception cref="ArgumentOutOfRangeException">When <paramref name="tag"/> is less than 252 or greater than 254.</exception>
         public FullFrame(
             byte tag,
             ushort offsetDelta,
@@ -151,6 +239,7 @@ namespace Bali.Attributes
         {
             if (tag != 255)
                 throw new ArgumentOutOfRangeException(nameof(tag));
+            
             OffsetDelta = offsetDelta;
             Locals = locals;
             Stack = stack;
@@ -162,11 +251,17 @@ namespace Bali.Attributes
             get;
         }
         
+        /// <summary>
+        /// Gets the locals in the stack frame.
+        /// </summary>
         public IList<VerificationInfo> Locals
         {
             get;
         }
         
+        /// <summary>
+        /// Gets the stack items of the stack frame.
+        /// </summary>
         public IList<VerificationInfo> Stack
         {
             get;
--- src/Bali/Attributes/StackMapTableAttribute.cs
+++ src/Bali/Attributes/StackMapTableAttribute.cs
@@ -2,14 +2,25 @@ using System.Collections.Generic;
 
 namespace Bali.Attributes
 {
+    /// <summary>
+    /// An attribute which provides rich type information about the bytecode which is used for verification.
+    /// </summary>
     public sealed class StackMapTableAttribute : JvmAttribute
     {
+        /// <summary>
+        /// Creates a new <see cref="StackMapTableAttribute"/>.
+        /// </summary>
+        /// <param name="nameIndex">The index into the <see cref="ConstantPool"/> representing the name of the attribute.</param>
+        /// <param name="entries">The <see cref="StackMapFrame"/>s.</param>
         public StackMapTableAttribute(ushort nameIndex, IList<StackMapFrame> entries)
             : base(nameIndex)
         {
             Entries = entries;
         }
         
+        /// <summary>
+        /// Gets the <see cref="StackMapFrame"/>s.
+        /// </summary>
         public IList<StackMapFrame> Entries
         {
             get;
