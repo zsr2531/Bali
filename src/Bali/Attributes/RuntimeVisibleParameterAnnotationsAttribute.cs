70810e2b29efc09c7d13bc5b64c053f863d2536b
zsr2531 <warhaamer1337@gmail.com>
2021-04-12 14:46:37 +0200

    Add remaining annotation attributes

---
+++ src/Bali/Attributes/RuntimeVisibleParameterAnnotationsAttribute.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Bali.SourceGeneration;
+
+namespace Bali.Attributes
+{
+    [AutoReader]
+    [AutoWriter]
+    public sealed class RuntimeVisibleParameterAnnotationsAttribute : JvmAttribute
+    {
+        public RuntimeVisibleParameterAnnotationsAttribute(ushort nameIndex, IList<IList<AnnotationInfo>> parameterAnnotations)
+            : base(nameIndex)
+        {
+            ParameterAnnotations = parameterAnnotations;
+        }
+        
+        [CustomReaderConverter(@"
+            var ParameterAnnotations = new List<IList<AnnotationInfo>>(reader.ReadU1());
+            var annotationReader = new AnnotationInfoReader(reader);
+            for (int i = 0; i < ParameterAnnotations.Capacity; i++) {
+                var annotations = new List<AnnotationInfo>(reader.ReadU2());
+                for (int j = 0; i < annotations.Capacity; j++)
+                    annotations.Add(annotationReader.ReadAnnotationInfo());
+            }"
+        )]
+        [CustomWriterConverter(@"
+            writer.WriteU1((byte) attribute.ParameterAnnotations.Count);
+            var annotationWriter = new AnnotationInfoWriter(writer);
+            foreach (var parameter in attribute.ParameterAnnotations) {
+                writer.WriteU2((ushort) parameter.Count);
+                foreach (var annotation in parameter)
+                    annotationWriter.WriteAnnotationInfo(annotation);
+            }
+        ")]
+        public IList<IList<AnnotationInfo>> ParameterAnnotations
+        {
+            get;
+            set;
+        }
+    }
+}
