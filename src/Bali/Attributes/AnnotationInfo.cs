e8630aacff41d323fd9dbe0309a4d30c4a17f4e6
zsr2531 <warhaamer1337@gmail.com>
2021-04-16 22:09:32 +0200

    Start adding xmldoc

--- src/Bali/Attributes/AnnotationInfo.cs
+++ src/Bali/Attributes/AnnotationInfo.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 
 namespace Bali.Attributes
 {
+    /// <summary>
+    /// Represents a single run-time visible annotation on a program element.
+    /// </summary>
     public sealed class AnnotationInfo
     {
+        /// <summary>
+        /// Creates a new <see cref="AnnotationInfo"/>.
+        /// </summary>
+        /// <param name="typeIndex">The index into the <see cref="ConstantPool"/> representing a field descriptor representing the annotation type.</param>
+        /// <param name="elementValuePairs">The list of element-value pairs.</param>
         public AnnotationInfo(ushort typeIndex, IList<(ushort ElementNameIndex, ElementValue Value)> elementValuePairs)
         {
             TypeIndex = typeIndex;
             ElementValuePairs = elementValuePairs;
         }
 
+        /// <summary>
+        /// Gets or sets the index into the <see cref="ConstantPool"/>
+        /// representing a field descriptor representing the annotation type.
+        /// </summary>
         public ushort TypeIndex
         {
             get;
             set;
         }
         
+        /// <summary>
+        /// Gets or sets the list of element-value pairs where
+        /// <list type="bullet">
+        /// <item>
+        /// <description>
+        /// ElementNameIndex: An index into the <see cref="ConstantPool"/> representing a valid field descriptor
+        /// denoting the name of the annotation type.
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <description>
+        /// Value: The <see cref="ElementValue"/> of the pair.
+        /// </description>
+        /// </item>
+        /// </list>
+        /// </summary>
         public IList<(ushort ElementNameIndex, ElementValue Value)> ElementValuePairs
         {
             get;
@@ -24,24 +52,80 @@ namespace Bali.Attributes
         }
     }
 
+    /// <summary>
+    /// An enumeration of all types of <see cref="ElementValue"/>s.
+    /// </summary>
     public enum ElementValueTag : byte
     {
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>byte</c>.
+        /// </summary>
         Byte = (byte) 'B',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>char</c>.
+        /// </summary>
         Char = (byte) 'C',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>double</c>.
+        /// </summary>
         Double = (byte) 'D',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>float</c>.
+        /// </summary>
         Float = (byte) 'F',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>int</c>.
+        /// </summary>
         Integer = (byte) 'I',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>long</c>.
+        /// </summary>
         Long = (byte) 'L',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>short</c>.
+        /// </summary>
         Short = (byte) 'S',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>boolean</c>.
+        /// </summary>
         Boolean = (byte) 'Z',
-        
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ConstValue"/> with type <c>String</c>.
+        /// </summary>
         String = (byte) 's',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="EnumConstValue"/>.
+        /// </summary>
         Enum = (byte) 'e',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is a <see cref="ClassInfoValue"/>.
+        /// </summary>
         Class = (byte) 'c',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> an <see cref="AnnotationValue"/>.
+        /// </summary>
         AnnotationType = (byte) '@',
+
+        /// <summary>
+        /// Indicates that the <see cref="ElementValue"/> is an <see cref="ArrayValue"/>.
+        /// </summary>
         Array = (byte) '['
     }
 
+    /// <summary>
+    /// Provides a contract for annotation element values.
+    /// </summary>
     public abstract class ElementValue
     {
         private protected ElementValue(ElementValueTag tag)
@@ -49,6 +133,9 @@ namespace Bali.Attributes
             Tag = tag;
         }
 
+        /// <summary>
+        /// Gets the <see cref="ElementValueTag"/>.
+        /// </summary>
         public ElementValueTag Tag
         {
             get;
